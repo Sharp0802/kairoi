f7ef62f0e9b3a98679b70448cf40a7e088fe5912
Sharp0802 <sharp0802.business@gmail.com>
2025-10-01 21:49:52 +0900

    :sparkles: Add basic example

---
+++ examples/basic.rs
@@ -0,0 +1,27 @@
+use kairoi::{info, AddConsoleHandler, GlobalHandler, Progress, Span, SpanData, SpanScope};
+use std::time::Duration;
+use tokio::time::sleep;
+
+#[tokio::main]
+async fn main() {
+    let global_handler = GlobalHandler::builder().console_handler().build();
+
+    info!("Hello");
+
+    Span::scope(async |scope: SpanScope| {
+        let data = SpanData::default().with_name("Hello World".to_string());
+
+        scope.update(data.with_progress(Progress::new(100, 0)));
+        for i in 1..=100 {
+            if i % 10 == 0 {
+                info!("{}/100", i);
+            }
+
+            sleep(Duration::from_millis(50)).await;
+            scope.update(data.with_progress(Progress::new(100, i)));
+        }
+    })
+    .await;
+
+    global_handler.join();
+}
